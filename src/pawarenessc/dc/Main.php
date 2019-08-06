8dbb220edeb182a62b909d232217aef731d503d6
PawarenessC <35727789+PawarenessC@users.noreply.github.com>
2019-08-07 01:19:17 +0900

    Update Main.php

--- src/pawarenessc/dc/Main.php
+++ src/pawarenessc/dc/Main.php
@@ -14,4 +14,52 @@ use pocketmine\command\CommandSender;
 
 use pocketmine\event\player\PlayerChatEvent;
 
-class Main extends pluginBase implements Listener{}
+use bboyyu51\discord\Sender;
+
+class Main extends pluginBase implements Listener{
+	
+	public function onEnable(){
+		$this->getLogger()->info("=========================");
+ 		$this->getLogger()->info("DiscordChatを読み込みました");
+ 		$this->getLogger()->info("制作者: PawarenessC");
+		$this->getLogger()->info("ライセンス: NYSL Version 0.9982");
+		$this->getLogger()->info("http://www.kmonos.net/nysl/");
+		$this->getLogger()->info("バージョン:{$this->getDescription()->getVersion()}");
+		$this->getLogger()->info("");
+		$this->getLogger()->info("当プラグインはDiscordWebhookを使用しております");
+		$this->getLogger()->info("DiscordWebhook製作者: bboyyu51");
+ 		$this->getLogger()->info("=========================");
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		
+		$this->config = new Config( $this->getDataFolder() . "Config.yml", Config::YAML);
+		$this->getSender()->Send($this->config->get("サーバー開放"));
+	}
+	
+	public function onDisable(){
+		$this->getSender()->Send($this->config->get("サーバー閉鎖"));
+		
+		$this->getLogger()->info("=========================");
+ 		$this->getLogger()->info("DiscordChatを停止しています");
+ 		$this->getLogger()->info("制作者: PawarenessC");;
+		$this->getLogger()->info("バージョン:{$this->getDescription()->getVersion()}");
+ 		$this->getLogger()->info("=========================");
+	}
+		
+	/**
+ 	* @ignoreCancelled
+ 	*/
+	public function onChat(PlayerChatEvent $event){
+		$name = $event->getPlayer()->getName();
+		$nametag = $event->getPlayer()->getDisplayName();
+		$msg = $event->getMessage();
+		if($this->config->get("名前だけで送信する") == "true"){
+		$this->getSender()->Send("{$name}: {$msg}");
+		}else{
+			$this->getSender()->Send("{$nametag}: {$msg}");
+		}
+	}
+	
+	private function getSender(){
+		return new Sender($this->config->get("URL"));
+	}
+}
