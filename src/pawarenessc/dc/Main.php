0fa18bbc06931803c50793a454d33f2c9e026472
PawarenessC <35727789+PawarenessC@users.noreply.github.com>
2019-08-07 00:54:07 +0900

    Create Main.php

---
+++ src/pawarenessc/dc/Main.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace pawarenessc\dc;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+
+use pocketmine\Server;
+use pocketmine\Player;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use pocketmine\event\player\PlayerChatEvent;
+
+class Main extends pluginBase implements Listener{}
